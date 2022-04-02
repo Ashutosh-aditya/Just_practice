e924efb50759ff9c115f144da05648fd7131bf9d
Ashutosh-aditya <78680582+Ashutosh-aditya@users.noreply.github.com>
2022-04-02 23:00:13 +0530

    print_sorted_array

---
+++ ffile.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int tt=0;tt<t;tt++)
+    {
+        int n;
+        cin>>n;
+        int ar[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>ar[i];
+        }
+        sort(ar,ar+n);
+        for(int i=0;i<n;i++)
+        {
+            cout<<ar[i]<<"\t";
+        }
+         cout<<"\n";
+    }
+    return 0;
+}
